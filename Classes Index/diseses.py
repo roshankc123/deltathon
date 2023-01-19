0c8990f33e8764e20c7b3c2e0122ff4442152785
Bibek Chalise <bibekchalise@Bibeks-MacBook-Air.local>
2023-01-19 20:00:22 +0545

    maize done

---
+++ Classes Index/diseses.py
@@ -0,0 +1,8 @@
+apple_classes = {'Apple___Apple_scab': 0,
+ 'Apple___Black_rot': 1,
+ 'Apple___Cedar_apple_rust': 2,
+ 'Apple___healthy': 3},
+Maize_classes = {'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot': 0,
+ 'Corn_(maize)___Common_rust_': 1,
+ 'Corn_(maize)___Northern_Leaf_Blight': 2,
+ 'Corn_(maize)___healthy': 3}
