03057b7195c448fbb1dd21b750f218d66c86e0ab
Bibek Chalise <bibekchalise@Bibeks-MacBook-Air.local>
2023-01-20 00:15:27 +0545

    rice classes

--- Classes Index/diseses.py
+++ Classes Index/diseses.py
@@ -7,4 +7,7 @@ Maize_classes = {'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot': 0,
  'Corn_(maize)___Common_rust_': 1,
  'Corn_(maize)___Northern_Leaf_Blight': 2,
  'Corn_(maize)___healthy': 3},
+
 Potato_classes ={'Potato___Early_blight': 0, 'Potato___Late_blight': 1, 'Potato___healthy': 2}
+
+Rice_classes = {'Bacterialblight': 0, 'Blast': 1, 'Brownspot': 2, 'Tungro': 3}
